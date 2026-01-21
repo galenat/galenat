68fa4109a833ccaeaf72ce9c499e092198778dd2
galenat <galenat.info@gmail.com>
2026-01-22 00:50:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ document.querySelectorAll('.nav-links a').forEach(link => {
 
 // ===== CARGA DINÁMICA DE EXPERIENCIAS =====
 const activitiesGrid = document.querySelector('.activities-grid');
-const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSjjIOH4mZiejbyg3vbOMbq0BIcwXtG63yLp_7XMZwxYTrVtg9dS-gkthcjp2Xz4DZI0AyJRd8C9aww/pubhtml';
+const sheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSjjIOH4mZiejbyg3vbOMbq0BIcwXtG63yLp_7XMZwxYTrVtg9dS-gkthcjp2Xz4DZI0AyJRd8C9aww/pub?output=csv';
 
 // Convierte CSV a array de objetos, soportando comas dentro de comillas
 function csvToArray(csv) {
@@ -85,5 +85,6 @@ fetch(sheetUrl)
     });
   })
   .catch(err => console.error('Error cargando experiencias:', err));
+
 
 
